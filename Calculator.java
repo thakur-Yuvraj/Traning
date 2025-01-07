f0f454a485e0bf929757cdce64599fe50ebbfb6b
thakur-Yuvraj <yuvipatel224@gmail.com>
2025-01-07 10:52:40 +0530

    'le'

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+Public class Calculator {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Input in this format:-");
+		
+		int a = sc.nextInt();
+		char operation = sc.nextChar();
+		int b = sc.nextInt();
+		
+			
+	}
+}
